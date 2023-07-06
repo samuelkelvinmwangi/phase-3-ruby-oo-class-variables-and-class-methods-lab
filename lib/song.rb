08ffdda2162a4a785572642be8aa51e1f4115460
samuel <samuelkelvinmwangi@gmail.com>
2023-07-06 12:33:13 +0300

    making sure the count starts at 0 and also the array for gener gives out the number of genres in the respective genre

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,8 @@ class Song
     @@count = 0
     @@artist = []
     @@genre = []
+    @@genre_count = {}
+    @@artist_count = {}
 end
 
 def initialize(name, artist, genre)
@@ -14,6 +16,12 @@ def initialize(name, artist, genre)
     @@count +=1
     @@artist << artist
     @@genre << genre
+
+    @@genre_count[genre] ||= 0
+    @@genre_count[genre] += 1
+
+    @@artist_count[artist] ||= 0
+    @@artist_count[artist] += 1
 end
 
 def self.count
@@ -34,4 +42,10 @@ def self.genre_count
 
   def self.artist_count
     @@artist_count
+  end
+
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each { |genre| genre_count[genre] += 1 }
+    genre_count
   end
